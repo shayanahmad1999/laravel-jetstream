e2710da5c83fe4bf167013fe6d2da9af825f374d
shayanahmad1999 <shayanahmad235@gmail.com>
2025-07-17 14:10:05 +0500

    updated some tailwind class

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -13,7 +13,7 @@ if (!function_exists('display_message')) {
         $color = $colors[$type] ?? 'gray';
 
         return '
-    <div class="max-w-7xl mx-auto py-4 mt-4 sm:px-6 lg:px-8 rounded-2xl bg-' . $color . '-100 text-gray-50 dark:bg-' . $color . '-800 dark:text-gray-200">
+    <div class="max-w-7xl mx-auto py-4 mt-4 sm:px-6 lg:px-8 rounded-2xl bg-' . $color . '-100 text-' . $color . '-50 dark:bg-' . $color . '-500 dark:text-' . $color . '-200">
         ' . $message . '
     </div>
     ';
